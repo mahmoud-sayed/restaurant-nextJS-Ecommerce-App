c6fac96d74defb06232a20769f1f35a07bc3ac91
mahmoud-sayed <mahmoudalsayied@gmail.com>
2023-02-02 14:37:27 +0200

    created the models

--- models/Product.js
+++ models/Product.js
@@ -2,5 +2,41 @@ import mongoose from "mongoose";
 
 
 const ProductSchema = new mongoose.Schema({
-  title: {}
-});
+  title: {
+    type: String,
+    required: true,
+    maxLength: 60
+  },
+  description: {
+    type: String,
+    required: true,
+    maxLength: 200
+  },
+  img: {
+    type: String,
+    required: true,
+  },
+  prices: {
+    type: [number],
+    required: true,
+  },
+  extraOptions: {
+    type: [
+      {
+        text: {
+          type: String,
+          required: true
+        },
+        price: {
+          type: Number,
+          required: true
+        }
+      }
+    ]
+  },
+},
+  { timestamps: true }
+);
+
+// here we export the collection Product with structured document (ProductSchema) and we check if it's already have bean created before or not
+export default mongoose.Model.Product || mongoose.Model("Product", ProductSchema);
